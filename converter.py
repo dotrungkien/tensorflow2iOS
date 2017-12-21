15191fa9104e01151995d775a430692949202119
Do Trung Kien <trungkien.keio@gmail.com>
2017-12-21 16:57:41 +0700

    update model

--- converter.py
+++ converter.py
@@ -3,4 +3,6 @@ import tfcoreml as tf_converter
 
 tf_converter.convert(tf_model_path='inception_v1_2016_08_28_frozen.pb',
                      mlmodel_path='InceptionV1.mlmodel',
-                     output_feature_names=['InceptionV1/Logits/Predictions/Softmax:0'])
+                     output_feature_names=['InceptionV1/Logits/Predictions/Softmax:0'],
+                     image_input_names='input:0',
+                     class_labels='imagenet_slim_labels.txt')
